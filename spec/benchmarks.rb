cd2fbff98bfb01a6a2072d2cbe5debecf1ce8777
Michael Ries <michael@riesd.com>
2014-01-20 10:36:11 -0700

    creating more benchmark data

--- spec/benchmarks.rb
+++ spec/benchmarks.rb
@@ -1,23 +1,24 @@
 require 'bundler/setup'
-require 'java_css_inliner'
+require 'derelicte'
 require 'benchmark'
 require 'fileutils'
 
 files_dir = File.join(File.dirname(__FILE__), 'files')
-html_file = File.join(files_dir, 'ink_boilerplate.html')
+html_file = File.join(files_dir, 'ink_hero.html')
 css_file  = File.join(files_dir, 'ink.css')
+css_file2 = File.join(files_dir, 'ink_hero.css')
 html_str = File.read(html_file)
-css_str  = File.read(css_file)
+css_str  = File.read(css_file) + File.read(css_file2)
 
 output_dir = File.join(File.dirname(__FILE__), '..', 'tmp')
-output_file = File.join(output_dir, 'ink_boilerplate.inlined.html')
+output_file = File.join(output_dir, 'ink_hero.inlined.html')
 FileUtils.mkdir_p(output_dir)
 
-inliner = CSS::Inliner.new
+inliner = Derelicte::Inliner.new
 
 # Get an initial benchmark
 Benchmark.bm(40) do |bm|
-  bm.report('initial run') do
+  bm.report('initial run (1x)') do
     inliner.inline(html_str, css_str)
   end
 end
@@ -28,13 +29,41 @@ File.open(output_file, 'w') do |f|
 end
 
 # Warmup the JIT
-500.times do
+1000.times do
   inliner.inline(html_str, css_str)
 end
 
 # Benchmark the JIT optimized time
 Benchmark.bm(40) do |bm|
-  bm.report('after warmup') do
-    inliner.inline(html_str, css_str)
+  bm.report('after warmup (10x)') do
+    10.times do
+      inliner.inline(html_str, css_str)
+    end
+  end
+end
+
+doc = ::Derelicte.doc_from_str(html_str)
+analyzer = ::Derelicte.css_analyzer_from_str(css_str)
+job = ::Derelicte::InlinerJob.new(doc, analyzer)
+job.send(:assignments)
+
+Benchmark.bm(40) do |bm|
+  bm.report('parse css to analyzer (10x)') do
+    10.times do
+      analyzer = ::Derelicte.css_analyzer_from_str(css_str)
+    end
+  end
+
+  bm.report('evaluate rules for doc (10x)') do
+    10.times do
+      job = ::Derelicte::InlinerJob.new(doc, analyzer)
+      job.send(:assignments)
+    end
+  end
+
+  bm.report('apply rules to doc (10x)') do
+    10.times do
+      job.apply_rules_to_doc
+    end
   end
 end
